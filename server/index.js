686bbb814f8574a8ebc91eb8fc8a5eb6fa89cf38
cgregs32 <cgregs32@gmail.com>
2018-02-15 12:09:07 -0700

    wire up mongoose to express API

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 //no access to es2015 modules
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
@@ -9,3 +13,4 @@ require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 console.log(`Server is listening on port: ${PORT}`);
+console.log('Visit:', `http://localhost:${PORT}`);
