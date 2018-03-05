fea90e6257cb49ffa3efd801c7399ad7e08c20d5
cgregs32 <cgregs32@gmail.com>
2018-03-05 11:50:40 -0700

    implement browser router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={Landing}/>
+      <Route exact path="/head" component={Header}/>
+      <Route exact path="/dash" component={Dashboard}/>
+      <Route exact path="/surveys" component={SurveyNew}/>
+    </Switch>
+  )
 };
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import {createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom'
+
 
 import App from './components/App';
 import reducers from './reducers'
@@ -10,7 +12,9 @@ const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.querySelector('#root')
 );
